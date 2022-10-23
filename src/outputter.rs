45b9d012995a2c6967fee484e26a5ade93692e4e
piltom <22397970+piltom@users.noreply.github.com>
2022-10-23 22:43:54 +0200

    add: is ip local + prettier format

--- src/outputter.rs
+++ src/outputter.rs
@@ -1,6 +1,6 @@
 use indicatif::{ProgressBar, ProgressStyle};
-use std::{fs, io::Write};
-use trust_dns_resolver::lookup_ip::LookupIp;
+use std::{fs, io::Write, net::IpAddr};
+use trust_dns_resolver::{lookup_ip::LookupIp, proto::rr::Record};
 
 pub trait Outputter {
     fn print_headers(&mut self) -> Result<(), std::io::Error>;
@@ -30,20 +30,19 @@ impl Outputter for FileOutput {
         if !self.headers {
             return Ok(());
         }
-        self.file.write("Name\t\t\tRecord Type\n".as_bytes())?;
+        self.file.write(
+            format!(
+                "{0: <40} | {1: <10} | {2: <10}\n",
+                "Name", "Record", "Local ip"
+            )
+            .as_bytes(),
+        )?;
 
         Ok(())
     }
     fn add_result(&mut self, lookup: LookupIp) -> Result<(), std::io::Error> {
         for record in lookup.as_lookup().records() {
-            self.file.write(
-                format!(
-                    "{}\t\t\t{}\n",
-                    record.name().to_ascii(),
-                    record.record_type()
-                )
-                .as_bytes(),
-            )?;
+            self.file.write(record_to_string(record, true).as_bytes())?;
         }
         Ok(())
     }
@@ -74,15 +73,16 @@ impl Outputter for ConsoleOutput {
         if !self.headers {
             return Ok(());
         }
-        println!("Name\t\t\tRecord Type");
+        println!(
+            "{0: <40} | {1: <10} | {2: <10}",
+            "Name", "Record", "Local ip"
+        );
         Ok(())
     }
 
     fn add_result(&mut self, lookup: LookupIp) -> Result<(), std::io::Error> {
         for record in lookup.as_lookup().records() {
-            println!(
-                "{}\t\t\t{}", record.name().to_ascii(), record.record_type()
-            );
+            println!("{}", record_to_string(record, false));
         }
         Ok(())
     }
@@ -90,5 +90,32 @@ impl Outputter for ConsoleOutput {
     fn report_progress(&self, text: String) {
         self.bar.inc(1);
         self.bar.set_message(text);
+    }
+}
+
+fn record_to_string(record: &Record, add_nl: bool) -> String {
+    let mut local = false;
+    if let Some(rdata) = record.data() {
+        local = match rdata.to_ip_addr() {
+            Some(IpAddr::V4(addrv4)) => addrv4.is_private(),
+            // Some(IpAddr::V6(addrv6)) => !addrv6.is_global(),  <= not stabilized in ip libs
+            _ => false,
+        };
+    };
+
+    if add_nl {
+        format!(
+            "{0: <40} | {1: <10} | {2: <10}\n",
+            record.name().to_ascii(),
+            record.record_type().to_string(),
+            local
+        )
+    } else {
+        format!(
+            "{0: <40} | {1: <10} | {2: <10}",
+            record.name().to_ascii(),
+            record.record_type().to_string(),
+            local
+        )
     }
 }
